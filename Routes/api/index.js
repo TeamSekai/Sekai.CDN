1fa0f914b0f46460147002df1ca3d96a203a4fec
ringoXD <nep360i@gmail.com>
2024-06-15 13:01:27 +0900

    ?offset = x

--- Routes/api/index.js
+++ Routes/api/index.js
@@ -19,8 +19,13 @@ router.use('/', fileUpload());
 router.use(async (req, res, next) => {
     if (req.method != "GET") return next();
     if (req.path.startsWith("/api/files")) return next();
+    const offset = parseInt(req.query.offset) || 0; // クエリパラメータから offset を取得する
     try {
-        res.json(await getDirectoryEntries(decodeURIComponent(req.path).slice(1).split("/").slice(1).join("/")));
+        let files = await getDirectoryEntries(decodeURIComponent(req.path).slice(1).split("/").slice(1).join("/"))
+        if (offset !== 0) {
+            files = files.slice(offset, offset + 10);
+        }
+        res.json(files);
     } catch (e) {
         res.sendStatus(404);
     }
